b1f550219dbc476735af4786cc3e6bf0a7204cdc
Eduardo Santos <edu.sidnei@hotmail.com>
2020-03-24 21:26:58 -0300

    atualizado

--- exemplo1.py
+++ exemplo1.py
@@ -1,16 +1,26 @@
-# Python Program to find the area of triangle
+""" ### AULA - INTRODUÇÃO AO PYTHON
+
+print(1 + 1)
+print(5 - 8)
+#POTENCIAÇÃO **  2**2 
+print(2 ** 5)
+
+# COMPARAÇÕES (==)
+print(9**2 == 80+1)
+print(9**2 != 80+1)
+print(10>10)
+### """
+
+""" ### AULA - VARIAVEIS E PRIMEIRO PROGRAMA
+x = 5
+print(x)
+
+frase = "A massa ainda comerá do fino biscoito que fabrica."
+print(frase)
 
 a = 5
-b = 6
-c = 8
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
+b = 3
+soma = a + b
+print ("A soma dos valores é",soma)
+### """
 
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
